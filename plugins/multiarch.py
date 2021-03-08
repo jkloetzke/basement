7073d27d0ffc588e30c50981b97dd8376f34862b
Jan Klötzke <jan@kloetzke.net>
2021-03-08 18:07:07 +0100

    checkpoint

--- plugins/multiarch.py
+++ plugins/multiarch.py
@@ -2,6 +2,8 @@ from bob.errors import ParseError
 import fnmatch
 import platform
 
+SUPPORTED_OS = ("linux-gnu", "msys", "win32")
+
 # TODO: support more architectures; support musl/dietlibc
 
 # get host architecture
@@ -20,9 +22,24 @@ def hostArch(args, **options):
     else:
         raise ParseError("Unsupported host machine: " + m)
 
+def hostMachine():
+    m = platform.uname().machine
+    if m == "x86_64":
+        return m
+    elif m == "AMD64":
+        return "x86_64"
+    elif m.startswith("i"):
+        return "i386"
+    elif m.startswith("aarch64"):
+        return m
+    elif m.startswith("arm"):
+        return m
+    else:
+        raise ParseError("Unsupported host machine: " + m)
+
 # get host autoconf triple
 def hostAutoconf(args, **options):
-    machine = hostArch(None)
+    machine = hostMachine()
     system = platform.uname().system
     if system == 'Linux':
         return machine + "-linux-gnu"
@@ -34,16 +51,27 @@ def hostAutoconf(args, **options):
         raise ParseError("Unsupported system: " + system)
 
 # set or replace vendor field in autoconf triplet
-def genAutoconf(args, **options):
-    if len(args) != 1:
-        raise ParseError("$(gen-autoconf,vendor) expects one argument")
-    machine, _, system = hostAutoconf(None).partition("-")
-    return machine + '-' + args[0] + '-' + system
+def autoconfSetVendor(args, **options):
+    if len(args) != 2:
+        raise ParseError("$(autoconf-set-vendor,triplet,vendor) expects two arguments")
+
+    # split off machine
+    machine, _, rest = args[0].partition("-")
+
+    # Determine OS. This is ambigous with the vendor field. Hence the list.
+    for vendor in SUPPORTED_OS:
+        if rest.endswith(vendor):
+            system = vendor
+            break
+    else:
+        raise ParseError("autoconf-set-vendor: unsupported triplet: " + args[0])
+
+    return machine + '-' + args[1] + '-' + system
 
 manifest = {
     'apiVersion' : "0.15",
     'stringFunctions' : {
-        "gen-autoconf" : genAutoconf,
+        "autoconf-set-vendor" : autoconfSetVendor,
         "host-arch" : hostArch,
         "host-autoconf" : hostAutoconf,
     }
